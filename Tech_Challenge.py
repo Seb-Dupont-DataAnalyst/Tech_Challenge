c41f4667e570d281882f8d447db23fffff6fe2da
Sebastien Dupont <79512309+Seb-Dupont-DataAnalyst@users.noreply.github.com>
2021-06-15 18:12:08 +0200

    Update Tech_Challenge.py

--- Tech_Challenge.py
+++ Tech_Challenge.py
@@ -31,11 +31,11 @@ st.set_page_config(layout='wide')
 # Titre principale
 #components.html("<body style='color:white;font-family:verdana; font-size:60px; border: 2px solid white; text-align: center; padding: 1px'><b>Cinéma Le Creusois</b></body>")
 st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
-st.markdown('<body class="title">Tech Challenge - Developper Data Science</body>', unsafe_allow_html=True)
+st.markdown('<body class="title">Tech Challenge - Data Science Developer</body>', unsafe_allow_html=True)
 
 # Création Sidebar avec les différents choix
 
-choice = st.sidebar.radio("", ('Accueil', "Analyse descriptive exploratoire (EDA)", 'Trouver la boutique correspondante',"Explication de l'impact de la météo",'Prévisions de ventes'
+choice = st.sidebar.radio("", ('Accueil', "1. Analyse descriptive exploratoire (EDA)", '2. Trouver la boutique correspondante',"3. Explication de l'impact de la météo",'4. Prévisions de ventes'
 ))
 
 df_sales['DATE'] = pd.to_datetime(df_sales['DATE']).dt.strftime('%Y-%m-%d')
@@ -66,7 +66,7 @@ if choice == 'Accueil':
 
 
 
-if choice == "Analyse descriptive exploratoire (EDA)":
+if choice == "1. Analyse descriptive exploratoire (EDA)":
 
     st.write("")
     st.write("")
@@ -98,7 +98,7 @@ if choice == "Analyse descriptive exploratoire (EDA)":
     fig1 = px.box(df_sales, color="ITEM",y="SALES")
     fig1.update_layout(
     title={
-            'text': "Répartition des données par item",
+            'text': "Répartition des données par produit",
             'y':0.9,
             'x':0.5,
             'xanchor': 'center',
@@ -108,6 +108,7 @@ if choice == "Analyse descriptive exploratoire (EDA)":
     fig1.update_xaxes(showgrid=False, gridwidth=1, gridcolor='black')
     fig1.update_yaxes(showgrid=False, gridwidth=1, gridcolor='black')
     fig1.update_layout(xaxis_title = "Produits", yaxis_title = "Ventes")
+    fig1.update_layout(width=1400,height=650)
     st.write(fig1)
     st.markdown('<body class="p4">On remarque que les ventes du produit B sont plus importantes que celle du produit A. La médiane se situe autour de 115 produits vendus par jour contre 67.</body>', unsafe_allow_html=True)
 
@@ -115,11 +116,19 @@ if choice == "Analyse descriptive exploratoire (EDA)":
     st.write("")
 
     fig3 = px.area(df_sales, x="DATE",y="SALES", color="ITEM")
+    fig3.update_layout(
+    title={
+            'text': "Evolution des ventes sur l'année 2019",
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'bottom'})
     fig3.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)',
                        'paper_bgcolor': 'rgba(0,0,0,0)', })
     fig3.update_xaxes(showgrid=False, gridwidth=1, gridcolor='black')
     fig3.update_yaxes(showgrid=False, gridwidth=1, gridcolor='black') 
     fig3.update_layout(xaxis_title = "Mois", yaxis_title = "Ventes")
+    fig3.update_layout(width=1400,height=650)
     st.write(fig3)
 
     st.write("")
@@ -138,10 +147,11 @@ if choice == "Analyse descriptive exploratoire (EDA)":
     fig2.update_xaxes(showgrid=False, gridwidth=1, gridcolor='black')
     fig2.update_yaxes(showgrid=False, gridwidth=1, gridcolor='black') 
     fig2.update_layout(xaxis_title = "Mois", yaxis_title = "Ventes")
+    fig2.update_layout(width=1400,height=650)
     st.write(fig2)
     st.markdown("<body class='p4'>La saisonnalité des 2 produits n'est pas la même. On a un produit A qui se vend plus l'hiver (de novembre à janvier) et un produit B qui se vend beaucoup plus l'été (juin à septembre)</body>", unsafe_allow_html=True)
 
-elif choice == 'Trouver la boutique correspondante':
+elif choice == '2. Trouver la boutique correspondante':
     st.write('')
     st.write('')
     st.write('')
@@ -152,7 +162,7 @@ elif choice == 'Trouver la boutique correspondante':
     st.write('')
     st.write('Pour retrouver la boutique qui correspond à cet historique de ventes, nous allons nous intéresser aux coefficients de corrélation.')
     st.write('Les ventes étant liées à la météo, on devrait pouvoir isoler des valeurs plus fortes (ou plus faibles) dans une ville par rapport aux autres.')
-    st.write("On s'intéresse ici à la première ligne qui fait ressortir les variables les plus fortement corrélés avec le produit A (tableau de gauche) et avec le produit B (tableau de droite")
+    st.write("On s'intéresse ici à la première ligne (ou la première colonne) qui fait ressortir les variables les plus fortement corrélés avec le produit A (tableau de gauche) et avec le produit B (tableau de droite)")
     st.write('')
     st.write('')
     
@@ -169,7 +179,8 @@ elif choice == 'Trouver la boutique correspondante':
         coeffc=np.corrcoef(df_sales_bordeaux[pmatrix].values.T)
 
         fig, axes = plt.subplots(figsize=(12, 5))
-        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values)
+        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values, vmin=0, vmax=1, cmap="YlGnBu")
+        sns.set(rc={'figure.facecolor':'white'})
         st.write('Produit A :')
         st.write(fig)
     
@@ -180,7 +191,8 @@ elif choice == 'Trouver la boutique correspondante':
         coeffc2=np.corrcoef(df_sales_bordeaux[pmatrix2].values.T)
 
         fig, axes = plt.subplots(figsize=(12, 5))
-        sns.heatmap(coeffc2, annot=True, yticklabels=pmatrix2.values, xticklabels=pmatrix2.values)
+        sns.heatmap(coeffc2, annot=True, yticklabels=pmatrix2.values, xticklabels=pmatrix2.values, vmin=0, vmax=1, cmap="YlGnBu")
+        sns.set(rc={'figure.facecolor':'white'})
         st.write('Produit B :')
         st.write(fig)
         
@@ -197,7 +209,7 @@ elif choice == 'Trouver la boutique correspondante':
         coeffc=np.corrcoef(df_sales_lyon[pmatrix].values.T)
 
         fig, axes = plt.subplots(figsize=(12, 5))
-        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values)
+        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values, vmin=0, vmax=1, cmap="YlGnBu")
         st.write('Produit A :')
         st.write(fig)
 
@@ -207,7 +219,7 @@ elif choice == 'Trouver la boutique correspondante':
         coeffc=np.corrcoef(df_sales_lyon[pmatrix].values.T)
 
         fig, axes = plt.subplots(figsize=(12, 5))
-        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values)
+        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values, vmin=0, vmax=1, cmap="YlGnBu") 
         st.write('Produit B :')
         st.write(fig)
 
@@ -224,7 +236,7 @@ elif choice == 'Trouver la boutique correspondante':
         coeffc=np.corrcoef(df_sales_lille[pmatrix].values.T)
 
         fig, axes = plt.subplots(figsize=(12, 5))
-        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values)
+        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values, vmin=0, vmax=1, cmap="YlGnBu")
         st.write('Produit A :')
         st.write(fig)
 
@@ -234,7 +246,7 @@ elif choice == 'Trouver la boutique correspondante':
         coeffc=np.corrcoef(df_sales_lille[pmatrix].values.T)
 
         fig, axes = plt.subplots(figsize=(12, 5))
-        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values)
+        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values, vmin=0, vmax=1, cmap="YlGnBu")
         st.write('Produit B :')
         st.write(fig)
 
@@ -252,7 +264,7 @@ elif choice == 'Trouver la boutique correspondante':
         coeffc=np.corrcoef(df_sales_marseille[pmatrix].values.T)
 
         fig, axes = plt.subplots(figsize=(12, 5))
-        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values)
+        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values, vmin=0, vmax=1, cmap="YlGnBu")
         st.write('Produit A :')
         st.write(fig)
 
@@ -262,7 +274,7 @@ elif choice == 'Trouver la boutique correspondante':
         coeffc=np.corrcoef(df_sales_marseille[pmatrix].values.T)
 
         fig, axes = plt.subplots(figsize=(12, 5))
-        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values)
+        sns.heatmap(coeffc, annot=True, yticklabels=pmatrix.values, xticklabels=pmatrix.values, vmin=0, vmax=1, cmap="YlGnBu")
         st.write('Produit B :')
         st.write(fig)
 
@@ -273,7 +285,7 @@ elif choice == 'Trouver la boutique correspondante':
     st.write("En effet, on remarque une nette différence pour le produit A avec les corrélations des autres boutiques pour ce produit.")
 
 
-elif choice == "Explication de l'impact de la météo":
+elif choice == "3. Explication de l'impact de la météo":
     st.write("")
     st.write("")
     st.write("")
@@ -282,12 +294,13 @@ elif choice == "Explication de l'impact de la météo":
     st.write("")
     st.write("")
     st.write("")
-    st.write("Voyons maintenant le poids de chaque critère météo dans l'évolution des ventes :")
-    st.write("")
+    st.write("Voyons maintenant le poids de chaque critère météo dans l'évolution des ventes : :thermometer:")
     st.write("")
 
     st.markdown("<body class='p3'>Produit A</body>", unsafe_allow_html=True)
     st.write('')
+    st.write("")
+    st.write("")
     col1, col2 = st.beta_columns(2)
     with col1:
 
@@ -296,7 +309,7 @@ elif choice == "Explication de l'impact de la météo":
         corr_pairs = correlation_mat.unstack()['A'].sort_values(ascending=False).iloc[1:10]
         st.write('Plus fortes corrélations positives :')
         st.write(corr_pairs)
-
+        st.write("Les principales variables impactant positivement les ventes du produit A sont donc les précipitations, le code de température de l'après-midi et le taux de couverture nuageuse.")
     st.write('')
 
     with col2:
@@ -306,11 +319,16 @@ elif choice == "Explication de l'impact de la météo":
         corr_pairs = correlation_mat.unstack()['A'].sort_values(ascending=True).iloc[1:10]
         st.write('Plus fortes corrélations négatives :')
         st.write(corr_pairs)
+        st.write("Les principales variables impactant négativement les ventes du produit A sont donc la durée d'ensoleillement, l'index de chaleur maximale et la température de l'après-midi.")
+        
 
-        st.write('')
+    st.write('')
+    st.write("")
 
     st.markdown("<body class='p3'>Produit B</body>", unsafe_allow_html=True)
     st.write('')
+    st.write("")
+    st.write("")
     col1, col2 = st.beta_columns(2)
     with col1:
         correlation_mat2 = df_sales_bordeaux.corr()
@@ -318,22 +336,26 @@ elif choice == "Explication de l'impact de la météo":
         corr_pairs2 = correlation_mat2.unstack()['B'].sort_values(ascending=False).iloc[1:10]
         st.write('Plus fortes corrélations positives :')
         st.write(corr_pairs2)
-
+        st.write("Les principales variables impactant positivement les ventes du produit B sont donc la température de l'après-midi, l'index de chaleur maximale et la température maximale.")
     with col2:
         correlation_mat2 = df_sales_bordeaux.corr()
 
         corr_pairs2 = correlation_mat2.unstack()['B'].sort_values(ascending=True).iloc[1:10]
         st.write('Plus fortes corrélations négatives :')
         st.write(corr_pairs2)
+        st.write("Les principales variables impactant négativement les ventes du produit B sont donc les précipitations, le code de température de l'après-midi et le taux de couverture nuageuse.")
 
     st.write("")
     st.write("")
+    st.markdown("<body class='p'>Conclusion</body>", unsafe_allow_html=True)
+    st.write("")
+    st.write("")    
     st.write("Contrairement à ce que pense Bernardo, le vent n'est pas la variable qui impacte le plus les ventes.")
     st.write("La vitesse du vent a bien un impact sur les ventes des produits A et B mais ce n'est pas l'élément qui joue le plus.")
     st.write("La température du vent impacte les ventes des 2 produits également mais là aussi, ce n'est pas l'élément qui joue le plus.")
    
 
-elif choice == "Prévisions de ventes":
+elif choice == "4. Prévisions de ventes":
     st.write("")
     st.write("")
     st.write("")
@@ -401,20 +423,20 @@ elif choice == "Prévisions de ventes":
 
 
     dict_MAE_A = {'KNN' : metrics.mean_absolute_error(y, modelKNN.predict(X)),'Linear Regression' : metrics.mean_absolute_error(y, model.predict(X)),
-              'Logistic Regression' : metrics.mean_absolute_error(y, model_LogReg.predict(X)), 'Gradient Boosting': metrics.mean_absolute_error(y, model_gbr.predict(X)), 
-              'XGBoost' : metrics.mean_absolute_error(y, model_XGB.predict(X)), 'Lasso' : metrics.mean_absolute_error(y, model_lasso.predict(X))}
+                'Logistic Regression' : metrics.mean_absolute_error(y, model_LogReg.predict(X)), 'Gradient Boosting': metrics.mean_absolute_error(y, model_gbr.predict(X)), 
+                'XGBoost' : metrics.mean_absolute_error(y, model_XGB.predict(X)), 'Lasso' : metrics.mean_absolute_error(y, model_lasso.predict(X))}
 
-    dict_MAE_B = {'KNN': metrics.mean_absolute_error(y2, modelKNN2.predict(X)),'Linear Regression' : metrics.mean_absolute_error(y2, model2.predict(X)), 
-              'Logistic Regression' : metrics.mean_absolute_error(y2, model_LogReg2.predict(X)), 'Gradient Boosting': metrics.mean_absolute_error(y2, model_gbr2.predict(X)), 
-              'XGBoost' : metrics.mean_absolute_error(y2, model_XGB2.predict(X)), 'Lasso' : metrics.mean_absolute_error(y2, model_lasso2.predict(X))}
+    dict_MAE_B = {'KNN': metrics.mean_absolute_error(y2, modelKNN2.predict(X2)),'Linear Regression' : metrics.mean_absolute_error(y2, model2.predict(X2)), 
+                'Logistic Regression' : metrics.mean_absolute_error(y2, model_LogReg2.predict(X2)), 'Gradient Boosting': metrics.mean_absolute_error(y2, model_gbr2.predict(X2)), 
+                'XGBoost' : metrics.mean_absolute_error(y2, model_XGB2.predict(X2)), 'Lasso' : metrics.mean_absolute_error(y2, model_lasso2.predict(X2))}
 
     dict_RMSE_A = {'KNN' : np.sqrt(metrics.mean_squared_error(y, modelKNN.predict(X))), 'Linear Regression' : np.sqrt(metrics.mean_squared_error(y, model.predict(X))),
-               'Logistic Regression' : np.sqrt(metrics.mean_squared_error(y, model_LogReg.predict(X))), 'Gradient Boosting': np.sqrt(metrics.mean_squared_error(y, model_gbr.predict(X))), 
-               'XGBoost' : np.sqrt(metrics.mean_squared_error(y, model_XGB.predict(X))), 'Lasso' : np.sqrt(metrics.mean_squared_error(y, model_lasso.predict(X)))}
+                'Logistic Regression' : np.sqrt(metrics.mean_squared_error(y, model_LogReg.predict(X))), 'Gradient Boosting': np.sqrt(metrics.mean_squared_error(y, model_gbr.predict(X))), 
+                'XGBoost' : np.sqrt(metrics.mean_squared_error(y, model_XGB.predict(X))), 'Lasso' : np.sqrt(metrics.mean_squared_error(y, model_lasso.predict(X)))}
 
-    dict_RMSE_B = {'KNN' : np.sqrt(metrics.mean_squared_error(y, modelKNN2.predict(X))), 'Linear Regression' : np.sqrt(metrics.mean_squared_error(y2, model2.predict(X))),
-               'Logistic Regression' : np.sqrt(metrics.mean_squared_error(y2, model_LogReg2.predict(X))), 'Gradient Boosting': np.sqrt(metrics.mean_squared_error(y2, model_gbr2.predict(X))), 
-               'XGBoost' : np.sqrt(metrics.mean_squared_error(y2, model_XGB2.predict(X))), 'Lasso' : np.sqrt(metrics.mean_squared_error(y, model_lasso2.predict(X)))}
+    dict_RMSE_B = {'KNN' : np.sqrt(metrics.mean_squared_error(y2, modelKNN2.predict(X2))), 'Linear Regression' : np.sqrt(metrics.mean_squared_error(y2, model2.predict(X2))),
+                'Logistic Regression' : np.sqrt(metrics.mean_squared_error(y2, model_LogReg2.predict(X2))), 'Gradient Boosting': np.sqrt(metrics.mean_squared_error(y2, model_gbr2.predict(X2))), 
+                'XGBoost' : np.sqrt(metrics.mean_squared_error(y2, model_XGB2.predict(X2))), 'Lasso' : np.sqrt(metrics.mean_squared_error(y2, model_lasso2.predict(X2)))}
 
     values_MAE_A = list(dict_MAE_A.values())
     names_MAE_A = list(dict_MAE_A.keys())
@@ -462,7 +484,7 @@ elif choice == "Prévisions de ventes":
     fig.update_layout(width=1300,height=550)
     st.write(fig)
 
-    st.write("Le modèle Gradient Boosting est donc le modèle que l'on retient car il présente les valeurs d'écart les plus faibles pour les 2 produits et les 2 indicateurs")
+    st.write("On retient le modèle GradientBoosting qui apparait comme l'un des meilleurs modèles pour cet exercice")
     st.write('')
     st.write('')
     st.write('Voyons maintenant les prévisions de ventes calculées :')
@@ -471,8 +493,8 @@ elif choice == "Prévisions de ventes":
     df_predict = pd.read_csv("https://raw.githubusercontent.com/murpi/wilddata/master/test/forecast.csv")
 
     X = df_predict[['PRECIP_TOTAL_DAY_MM',	'CLOUDCOVER_AVG_PERCENT', 'HUMIDITY_MAX_PERCENT', 'MAX_TEMPERATURE_C',	'MIN_TEMPERATURE_C',	'WINDSPEED_MAX_KMH',	'VISIBILITY_AVG_KM',	'PRESSURE_MAX_MB']]
-    df_predict['predictions A'] = model_XGB.predict(X)
-    df_predict['predictions B'] = model_XGB2.predict(X)
+    df_predict['predictions A'] = model_gbr.predict(X)
+    df_predict['predictions B'] = model_gbr2.predict(X)
 
     df_predict['predictions A'] = df_predict['predictions A'].round(decimals=0)
     df_predict['predictions B'] = df_predict['predictions B'].round(decimals=0)
